1a511415ade209b40c46c49498801fbc872d17b4
faidra <faidra.windmark@gmail.com>
2021-07-12 00:21:35 +0900

    Rotationにも対応

--- OpenVR-SpaceCalibratorClientDLL/DllInterface.cpp
+++ OpenVR-SpaceCalibratorClientDLL/DllInterface.cpp
@@ -8,22 +8,25 @@ void Connect()
 	Driver.Connect();
 }
 
-void SetDeviceOffset(uint32_t id, double x, double y, double z)
+void SetDeviceTransform(uint32_t id, double x, double y, double z, double qx, double qy, double qz, double qw)
 {
 	vr::HmdVector3d_t pos;
 	pos.v[0] = x;
 	pos.v[1] = y;
 	pos.v[2] = z;
 
-	vr::HmdQuaternion_t zeroQ;
-	zeroQ.x = 0; zeroQ.y = 0; zeroQ.z = 0; zeroQ.w = 1;
+	vr::HmdQuaternion_t rot;
+	rot.x = qx;
+	rot.y = qy;
+	rot.z = qz;
+	rot.w = qw;
 
 	protocol::Request req(protocol::RequestSetDeviceTransform);
-	req.setDeviceTransform = { id, true, pos, zeroQ };
+	req.setDeviceTransform = { id, true, pos, rot };
 	Driver.SendBlocking(req);
 }
 
-void ResetAndDisableOffsets(uint32_t id)
+void ResetAndDisableDeviceTransform(uint32_t id)
 {
 	vr::HmdVector3d_t zeroV;
 	zeroV.v[0] = zeroV.v[1] = zeroV.v[2] = 0;
--- OpenVR-SpaceCalibratorClientDLL/DllInterface.h
+++ OpenVR-SpaceCalibratorClientDLL/DllInterface.h
@@ -6,6 +6,6 @@
 extern "C"
 {
 	__declspec(dllexport) void Connect();
-	__declspec(dllexport) void SetDeviceOffset(uint32_t id, double x, double y, double z);
-	__declspec(dllexport) void ResetAndDisableOffsets(uint32_t id);
+	__declspec(dllexport) void SetDeviceTransform(uint32_t id, double x, double y, double z, double qx, double qy, double qz, double qw);
+	__declspec(dllexport) void ResetAndDisableDeviceTransform(uint32_t id);
 }
